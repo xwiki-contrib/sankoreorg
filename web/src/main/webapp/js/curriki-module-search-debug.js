adfe2e9403b8505ea3a0792d9c0316b4fb70977c
flaviusx <flavius.olaru@gmail.com>
2011-09-05 11:16:14 +0000

    Added a new special filter for ressources
    git-svn-id: https://svn.xwikisas.com/svnroot/xwiki/projects/sankore/trunk@4341 d4d025b9-7bfe-4295-b4c0-7365c5b8ee40

--- web/src/main/webapp/js/curriki-module-search-debug.js
+++ web/src/main/webapp/js/curriki-module-search-debug.js
@@ -495,7 +495,7 @@ data.init = function(){
 
 	f.data.special = {
 		list: [
-			'contributions', 'collections', 'updated'
+			'contributions', 'collections', 'updated', 'sankore'
 		]
 		,data: [
 			['', _('search.resource.special.selector.UNSPECIFIED')]
