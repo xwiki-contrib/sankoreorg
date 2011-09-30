a8aa4215e1034aa7ccc7fd201049bce3af452711
flaviusx <flavius.olaru@gmail.com>
2011-09-30 15:52:06 +0000

    SKE-178 Commented add resource from template
    git-svn-id: https://svn.xwikisas.com/svnroot/xwiki/projects/sankore/trunk@4704 d4d025b9-7bfe-4295-b4c0-7365c5b8ee40

--- web/src/main/webapp/js/curriki-module-addpath-debug.js
+++ web/src/main/webapp/js/curriki-module-addpath-debug.js
@@ -311,11 +311,11 @@ Curriki.module.addpath.init = function(){
 							}
 
 		// Create with Template
-						},{
+						/*},{
 							 xtype:'radio'
 							,value:'template'
 							,inputValue:'template'
-							,boxLabel:_('add.contributemenu.option.template')
+							,boxLabel:_('add.contributemenu.option.template')*/
 
 		// Create "from scratch"
 						},{
