865d4e6b734cf2ae28ce8598b5bfe9b883ab3fb8
ldubost <ludovic@xwiki.com>
2012-09-03 16:52:46 +0000

    New version of sankore player
    git-svn-id: https://svn.xwikisas.com/svnroot/xwiki/projects/sankore/trunk@8535 d4d025b9-7bfe-4295-b4c0-7365c5b8ee40

--- web/src/main/webapp/skins/curriki20/uniboard-player/scripts/ubloader.js
+++ web/src/main/webapp/skins/curriki20/uniboard-player/scripts/ubloader.js
@@ -3,42 +3,54 @@ var UbLoader = {};
 UbLoader.playerprefix = "./uniboard-player";
 
 UbLoader.init = function(playerprefix) {
-  UbLoader.playerprefix = playerprefix;
-  // prototype compat mode
-  jQuery.noConflict();
+   UbLoader.playerprefix = playerprefix;
+   // prototype compat mode
+   jQuery.noConflict();
 }
 
-UbLoader.resize = function() {
-  var sankoreplayer = jQuery("#sankoreplayer");
-  console.log("LOG: sankoreplayer height " + sankoreplayer.height());
-  var sankoreheader = jQuery("#sankoreplayer-header");
-  console.log("LOG: sankoreplayer-header height " + sankoreheader.outerHeight());
-  var sankoreiframe = jQuery("#sankoreplayer-iframe");
-  console.log("LOG: sankoreplayer-iframe height " + sankoreiframe.height());
-  sankoreiframe.height(sankoreplayer.height() - sankoreheader.outerHeight());
-  sankoreiframe.css("marginTop", sankoreheader.outerHeight());
-  console.log("LOG: adjusted sankoreplayer-iframe height " + sankoreiframe.height());
-  sankoreplayer.css("position", "fixed");
-  jQuery("body").css("overflow", "hidden");
-}
+UbLoader.launchPlayer = function(file, nbpages, mode, elementid) {
+        // setting full screen
+        var myUbLoader = null;
 
-UbLoader.launchPlayer = function(file, nbpages) {
-  
-  jQuery(document).ready(function() {
-    var sankoreiframe = jQuery("#sankoreplayer-iframe");
-    sankoreiframe.attr("src", UbLoader.playerprefix + 'player.html#' + nbpages + ',' + file);
-    jQuery("#sankore-preview").hide();
-    jQuery("#sankoreplayer").show();
-    //jQuery(window).resize();
-    UbLoader.resize();
-    
-  });
+        UbLoader.mode = mode;
+        UbLoader.elementid = elementid;
+        jQuery(document).ready(function() {
+                var el = jQuery(elementid);
+  		el.append('<div id="sankoreplayer"><div id="sankoreplayer-header"><div><ul id="sankoreplayer-menulist"><li><a href="javascript:void(0)" onclick="UbLoader.closePlayer(); return false">Close</a></li></ul></div></div><iframe id="sankoreplayer-iframe" src="' + UbLoader.playerprefix + 'player.html#' + file + ',' + nbpages + ',' + mode + '" width="100%" height="100%" border="0" frameborder="0"></iframe></div>');
+ 		el.append('<link rel=stylesheet type="text/css" href="' + UbLoader.playerprefix + 'stylesheets/ubloader.css">');
+ 		el.append('<link rel=stylesheet type="text/css" href="' + UbLoader.playerprefix + 'stylesheets/ubloader-' + mode + '.css">');
+        }); 
+
+        jQuery(window).resize(function(){
+          // console.log("Player loader resize started");
+          if (UbLoader.mode=="fs") {
+             if (jQuery("#sankoreplayer-menulist").length>0) {      
+                 var top = jQuery("#sankoreplayer-header").height() + 6;
+                 var currentHeight = jQuery("#sankoreplayer-iframe").height();
+                 if (currentHeight != jQuery(window).height() - top) {
+                    jQuery("#sankoreplayer-iframe").height(jQuery(window).height() - top);
+                 }
+             }
+           }
+        }); 
+        jQuery(window).resize();
 } // end function launchPlayer
 
 UbLoader.closePlayer = function() {
-  // hidding the player  
-  jQuery("#sankoreplayer").hide();
-  jQuery("#sankore-preview").show();  
-  jQuery("body").css("overflow", "auto");
+    // hidding the player
+    jQuery("#sankoreplayer").remove();
 }
 
+UbLoader.switchMode = function(mode) {
+    if (mode=="fs") {
+      UbLoader.mode="fs";
+    } else if (mode=="embed") {
+      UbLoader.mode="embed";
+      // reverting iframe manual full screen setting
+      jQuery("#sankoreplayer-iframe").css("height", "");
+    }
+    var el = jQuery(UbLoader.elementid);
+    // reverting css to correct mode
+    el.append('<link rel=stylesheet type="text/css" href="' + UbLoader.playerprefix + 'stylesheets/ubloader-' + UbLoader.mode + '.css">');
+    jQuery(window).resize();
+}
--- web/src/main/webapp/skins/curriki20/uniboard-player/scripts/ubplayer.js
+++ web/src/main/webapp/skins/curriki20/uniboard-player/scripts/ubplayer.js
@@ -1,190 +1,165 @@
 var UbPlayer = {};
 
-(function ($) {
-$.fn.vAlign = function() {
-return this.each(function(i){
-var h = $(this).height();
-console.log("LOG: vAlign height: " + h);
-var oh = $(this).outerHeight();
-console.log("LOG: vAlign outerHeight: " + oh);
-var mt = (h + (oh - h)) / 2;
-console.log("LOG: vAlign marginTop:" + mt);
-$(this).css("margin-top", "-" + mt + "px");
-$(this).css("top", "50%");
-$(this).css("position", "absolute");
-});
-};
-})(jQuery);
-
-(function ($) {
-$.fn.hAlign = function() {
-return this.each(function(i){
-var w = $(this).width();
-console.log("LOG: hAlign width: " + w);
-var ow = $(this).outerWidth();
-console.log("LOG: hAlign outerWidth: " + ow);
-var ml = (w + (ow - w)) / 2;
-console.log("LOG: hAlign marginLeft: " + ml);
-$(this).css("margin-left", "-" + ml + "px");
-$(this).css("left", "50%");
-$(this).css("position", "absolute");
-});
-};
-})(jQuery);
-
 UbPlayer.playerprefix = "./uniboard-player";
-
-UbPlayer.myUbPlayer = null;
+UbPlayer.mode = "fs";
 
 UbPlayer.init = function(playerprefix) {
-  UbPlayer.playerprefix = playerprefix;
+   UbPlayer.playerprefix = playerprefix;
 }
 
-UbPlayer.resize = function() {
-  if (UbPlayer.myUbPlayer) {
-    console.log("LOG: Player resize started");
-    var ratioWh = 1.6;
-    console.log("LOG: Player image ratio: " + ratioWh);
-    
-    var head = jQuery("#head");
-    var top = head.is(":visible") ? head.outerHeight() : 0;
-    console.log("LOG: head height " + top);
-    var foot = jQuery("#foot");
-    var bottom = foot.is(":visible") ? foot.outerHeight() - 20 : 0;
-    console.log("LOG: foot height " + bottom);
-    var body = jQuery("body");
-    console.log("LOG: body height " + body.height());
-    var sankorebody = jQuery("#sankorebody");
-    console.log("LOG: sankorebody height " + sankorebody.height());
-    sankorebody.css("marginTop", top);
-    
-    var page = jQuery("#current-page");
-    
-    // setting sankorebody
-    sankorebody.height(body.height() - top - bottom);
-    console.log("LOG: adjusted sankorebody height " + sankorebody.height());
-    
-    // setting page      
-    if (UbPlayer.myUbPlayer.resizeMode === "V") {
-      console.log("LOG: page before height: " + page.height());
-      page.height(sankorebody.height() - 48);
-      console.log("LOG: page after height: " + page.height());
-      page.width(page.height() * ratioWh);
-      page.vAlign();
-      page.hAlign();
-    }
-    else 
-      if (UbPlayer.myUbPlayer.resizeMode === "H") {
-        page.width(sankorebody.width() - 76);
-        page.height(page.width() * (1 / ratioWh));
-      }
-  /*
-   if(jQuery("body").width() < (jQuery(".page").width() + 76) && myUbPlayer.resizeMode === "V") {
-   myUbPlayer.resizeMode = "H";
-   console.log("switch to H mode");
-   jQuery(window).resize();
-   } else if (jQuery("body").height() < (jQuery(".page").height() + paddingTop + paddingBottom) && myUbPlayer.resizeMode === "H") {
-   myUbPlayer.resizeMode = "V";
-   console.log("switch to V mode");
-   jQuery(window).resize();
-   }*/
-  }
-}
+UbPlayer.launchPlayer = function(file, nbpages, mode) {
+        console.log("Player opening");    
 
-jQuery(window).resize(function() {
-  UbPlayer.resize();
-});
+        var myUbPlayer = null;
 
-UbPlayer.launchPlayer = function(file, nbpages) {
-  console.log("Player opening");    
+        // set the mode to be used
+        UbPlayer.startMode = mode;
+        UbPlayer.mode = mode;
 
-  jQuery(document).ready(function() {
+        console.log("Player doc ready");    
+        jQuery(document).ready(function(){
 
-    var args = {
-      documentData: {
-        author: 'Ludovic',
-        authorEmail: 'ludovic@xwiki.com',
-        title:'Test',
-        description:'Test',
-        publishedAt:'Test',
-        uuid:'0000',
-        hasPdf:'false',
-        hasUbz:'false',
-        numberOfPages: nbpages,
-        pagesBaseUrl: file,
-        format: 'ubz'
-     },
-     pagesImg:[]
-    };
+        var args = {
+                documentData:{
+                author:'Ludovic',
+                authorEmail:'ludovic@xwiki.com',
+                title:'Test',
+                description:'Test',
+                publishedAt:'Test',
+                uuid:'0000',
+                hasPdf:'false',
+                hasUbz:'false',
+                numberOfPages: nbpages,
+                pagesBaseUrl: file 
+            },
+            pagesImg:[]
+        };
 
-    UbPlayer.myUbPlayer = new UbPlayer.Player(args);
+        console.log("Player initial load start");    
+        myUbPlayer = new UbPlayer.Player(args);
+        console.log("Player initial load done");    
 
-  // load specific stylesheets according to the window width
-	/*
-    if(jQuery("body").width() < 1000) {
-      console.log("Player mode embedded");    
+        // load specific stylesheets according to the window width
+        jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_' + mode + '.css">');
 
-      myUbPlayer.state = "embedded";
-      if(!jQuery.browser.msie) {
-        jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_embed.css">');
-      }else{
+        if(jQuery("body").width() < 1000){
+            console.log("Player mode embedded");    
 
-      }
-    } else {
-      if(!jQuery.browser.msie) {
-        console.log("Player non ie mode");    
+            myUbPlayer.state = "embedded";
+            if(!jQuery.browser.msie){
+                jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_embed.css">');
+            }else{
 
-        if(jQuery.browser.safari) {
-          jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_ipad.css">');
-          addSwipeListener(document.body, function(e) {
-            if(e.direction=="right") {
-              myUbPlayer.goToPage("PREVIOUS");
-            } else if(e.direction=="left") {
-              myUbPlayer.goToPage("NEXT");
             }
-          });
-        jQuery("#boards").addClass("boardsEnableAnimation");
-        }
-      } else {
-        console.log("Player ie mode");    
-
-        jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_ie.css">');
-
-        if(jQuery.browser.version != "8.0") {
-          jQuery("#alert").css({"display": "block"});
-          jQuery("#alert-background").animate({opacity:0.9},500);
-          jQuery("#alert-box").html('<span>You are running an old version of Internet Explorer. Please update your browser or install Google Chrome Frame. <a href="http://www.google.com/chromeframe">Download Google Chrome Frame</a></span>');
         } else {
-          jQuery("#alert").css({"display": "table"});
-          jQuery("#alert-background").animate({opacity:0.5},500);
-          jQuery("#alert-box").html('<span>Please start <b>Firefox, Safari</b> or <b>Chrome</b> to view this document, or <a href="http://www.google.com/chromeframe">Download the Google Chrome Frame plugin</a> to view it in Internet Explorer. (<a href="#">close</a>)</span>');
+            if(!jQuery.browser.msie){
+                console.log("Player non ie mode");    
+
+                if(jQuery.browser.safari) {
+                    jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_ipad.css">');
+                    addSwipeListener(document.body, function(e) {
+                        if(e.direction=="right"){
+                            myUbPlayer.goToPage("PREVIOUS");
+                        }else if(e.direction=="left"){
+                            myUbPlayer.goToPage("NEXT");
+                        }
+                    });
+                    jQuery("#boards").addClass("boardsEnableAnimation");
+                }
+            }else{
+                console.log("Player ie mode");    
+
+                jQuery("body").append('<link rel=stylesheet type="text/css" href="' + UbPlayer.playerprefix + 'stylesheets/master_ie.css">');
+/*
+                if(jQuery.browser.version != "8.0"){
+                    jQuery("#alert").css({"display": "block"});
+                    jQuery("#alert-background").animate({opacity:0.9},500);
+                    jQuery("#alert-box").html('<span>You are running an old version of Internet Explorer. Please update your browser or install Google Chrome Frame. <a href="http://www.google.com/chromeframe">Download Google Chrome Frame</a></span>');
+                }else{
+                    jQuery("#alert").css({"display": "table"});
+                    jQuery("#alert-background").animate({opacity:0.5},500);
+                    jQuery("#alert-box").html('<span>Please start <b>Firefox, Safari</b> or <b>Chrome</b> to view this document, or <a href="http://www.google.com/chromeframe">Download the Google Chrome Frame plugin</a> to view it in Internet Explorer. (<a href="#">close</a>)</span>');
+                }
+*/
+            }
         }
-      }
-    }*/
 
-    
+        jQuery(window).resize(function(){
+            if (UbPlayer.mode=="fs") {
+                console.log("Player resize started");    
+                var ratioWh = myUbPlayer.currentPage.ratio;
+                console.log("Player image ratio: " + ratioWh);    
 
-    jQuery("#alert").click(function(){
-      jQuery(this).hide();
-    });
+                var paddingTop = jQuery("#head").is(":visible") ? jQuery("#head").height() : 0;
+                var paddingBottom = jQuery("#foot").is(":visible") ? jQuery("#foot").height() : 0;
 
-    // remove unnecessary items
-    jQuery("#head-list-closeDescription").remove();
-    jQuery("#head-list-share").remove();
-    jQuery("#head-embed-box-left").remove();
-    jQuery("#menu-button-export").remove();
-    jQuery(".menu-box-right").remove();
+                console.log("Player start log");    
+                console.log("Player paddingTop: " + paddingTop);    
+                console.log("Player paddingBottom: " + paddingBottom);    
+                console.log("Player resizeMode: " + myUbPlayer.resizeMode);    
+                console.log("Player page width: " + jQuery(".page").width());    
+                console.log("Player page height: " + jQuery(".page").height());    
+                console.log("Player sankore body width: " + jQuery("#sankorebody").width());    
+                console.log("Player sankore body height: " + jQuery("#sankorebody").height());    
+                console.log("Player body width: " + jQuery("body").width());    
+                console.log("Player body height: " + jQuery("body").height());    
+                console.log("Player window width: " + jQuery(window).width());    
+                console.log("Player windows height: " + jQuery(window).height());    
 
-    // The current document has to be resized after the stylesheets have been loaded
-    
-    setTimeout(function() {
-      //jQuery("#head-embed").css({"display":"block"});
-      //jQuery("#sankorebody").css({"display":"block"});
-      jQuery(window).resize();
-    }, 500);
+                if(myUbPlayer.resizeMode === "V"){
+                console.log("before height: " + jQuery(".page").height());    
+                jQuery(".page").height(jQuery("body").height() - paddingTop - paddingBottom - 50);
+                jQuery(".page").css("margin-top", paddingTop + 25);
+                console.log("after height: " + jQuery(".page").height());    
+                jQuery(".page").width(jQuery(".page").height() * ratioWh);
+                }else if(myUbPlayer.resizeMode === "H"){
+                        jQuery(".page").width(jQuery("body").width() - 76);
+                        jQuery(".page").height(jQuery(".page").width() * (1/ratioWh));
+                }
 
-    console.log("Resize done");    
-  }); //resize
+                console.log("Player page width: " + jQuery(".page").width());    
+                console.log("Player page height: " + jQuery(".page").height());    
+                console.log("Player sankore body width: " + jQuery("#sankorebody").width());    
+                console.log("Player sankore body height: " + jQuery("#sankorebody").height());  
+                console.log("Player body width: " + jQuery("body").width());    
+                console.log("Player body height: " + jQuery("body").height());    
+                console.log("Player window width: " + jQuery(window).width());    
+                console.log("Player windows height: " + jQuery(window).height());    
 
-  console.log("Player activated");    
+                if(jQuery("body").width() < (jQuery(".page").width() + 76) && myUbPlayer.resizeMode === "V"){
+                    myUbPlayer.resizeMode = "H";
+                console.log("switch to H mode");
+                    jQuery(window).resize();
+                } else if(jQuery("body").height() < (jQuery(".page").height() + paddingTop + paddingBottom) && myUbPlayer.resizeMode === "H"){
+                    myUbPlayer.resizeMode = "V";
+                console.log("switch to V mode");
+                    jQuery(window).resize();
+                };
+             } // end if mode
+        });
+
+        jQuery("#alert").click(function(){
+            jQuery(this).hide();
+        });
+
+        // remove unnecessary items
+        jQuery("#head-list-closeDescription").remove();
+        jQuery("#head-list-share").remove();
+        jQuery("#head-embed-box-left").remove();
+     //   jQuery("#menu-button-export").remove();
+     //   jQuery(".menu-box-right").remove();
+
+        myUbPlayer.showHideThumbnails();
+
+        // The current document has to be resized after the stylesheets have been loaded
+        setTimeout(function() {
+              jQuery("#head-embed").css({"display":"block"});
+              jQuery("#sankorebody").css({"display":"block"});
+              jQuery(window).resize();
+              }, 500);
+
+        console.log("Resize done");    
+    }); //resize
+
+    console.log("Player activated");    
 } // end function launchPlayer
